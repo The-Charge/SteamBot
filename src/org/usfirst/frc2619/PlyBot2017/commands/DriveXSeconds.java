3ddd7474f42a8f4f4ad25fec2d651df26d111336
Andrew Menzl <Menzla@midlandps.org>
2017-01-19 19:50:29 -0500

    Created DriveXFeet command, added stop() in DriveTrain, had DriveXSeconds reference stop().

--- src/org/usfirst/frc2619/PlyBot2017/commands/DriveXSeconds.java
+++ src/org/usfirst/frc2619/PlyBot2017/commands/DriveXSeconds.java
@@ -50,7 +50,7 @@ public class DriveXSeconds extends TimedCommand {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.run(0, 0);
+    	Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
