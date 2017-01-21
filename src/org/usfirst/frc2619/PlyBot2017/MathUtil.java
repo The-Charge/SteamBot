9d9d1cc9cb543b14d12785ae94b6efe320aaf275
JessicaKline <jrkline1@outlook.com>
2017-01-21 15:51:05 -0500

    TurnNDegrees workable. Needs some fine tuning still

--- src/org/usfirst/frc2619/PlyBot2017/MathUtil.java
+++ src/org/usfirst/frc2619/PlyBot2017/MathUtil.java
@@ -11,4 +11,15 @@ public class MathUtil {
 		return Math.abs(input) < db ? 0 : input;
 		// --else-
 	}
+	public static double calcDirection(double current, double desired) {
+		current = Math.toRadians(current);
+		desired = Math.toRadians(desired);
+		double current_x = Math.cos(current);
+		double current_y = Math.sin(current);
+		double desired_x = Math.cos(desired);
+		double desired_y = Math.sin(desired);
+		double direction = Math.signum(current_x * desired_y - desired_x
+				* current_y);
+		return direction;
+	}
 }
