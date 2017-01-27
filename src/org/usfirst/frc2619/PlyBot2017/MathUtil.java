b22c59a95dd3bdc9f270f7b4290ea2c3155cf541
Colin Witt <realbuddyc56@gmail.com>
2017-01-27 16:54:07 -0500

    Added a multiplier constant to put the robot at max power at max joystick

--- src/org/usfirst/frc2619/PlyBot2017/MathUtil.java
+++ src/org/usfirst/frc2619/PlyBot2017/MathUtil.java
@@ -3,10 +3,16 @@ package org.usfirst.frc2619.PlyBot2017;
 public class MathUtil {
 	public static double delinearize(double input, int power) {
 		
+		double dbY = Robot.driveTrain.DEADBAND_Y;
+		
+		//returns delinearized power (adjusted for deadband)
 		if(input > 0)
-			return Math.pow(input - Robot.driveTrain.DEADBAND_Y, power);
+			//this is an equation for the curve so that it starts at zero at the edge of the deadband...
+			//...and goes up cubically to (1,1)
+			return 1/(1 - dbY) * Math.pow(input - dbY, power);
 		else if (input < 0)
-			return Math.pow(input + Robot.driveTrain.DEADBAND_Y, power);
+			//this is the reverse of that equation, in order to make the negative go backward
+			return 1/(1 - dbY) * Math.pow(input + dbY, power);
 		else
 			return 0;
 	}
