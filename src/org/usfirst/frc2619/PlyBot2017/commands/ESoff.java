f8a57dbb68900f26e3b4f2250e6025d6472693f1
abcurrie <abcurrie@gmail.com>
2017-02-04 12:00:22 -0500

    change commands to only execute once in initialize

--- src/org/usfirst/frc2619/PlyBot2017/commands/ESoff.java
+++ src/org/usfirst/frc2619/PlyBot2017/commands/ESoff.java
@@ -38,17 +38,17 @@ public class ESoff extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	ElectricalSolenoid.offES();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	ElectricalSolenoid.offES();
+   
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
