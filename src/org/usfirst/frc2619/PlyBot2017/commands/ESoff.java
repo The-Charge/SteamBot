fbb1283d3af80e2838a21f1059fbc821fb72abc5
abcurrie <abcurrie@gmail.com>
2017-02-03 22:28:15 -0500

    fix esoff command to use the off method

--- src/org/usfirst/frc2619/PlyBot2017/commands/ESoff.java
+++ src/org/usfirst/frc2619/PlyBot2017/commands/ESoff.java
@@ -43,7 +43,7 @@ public class ESoff extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	ElectricalSolenoid.fwdES();
+    	ElectricalSolenoid.offES();
     }
 
     // Make this return true when this Command no longer needs to run execute()
