cf3c25260b3e7fe03852a7b99d351e0511c3fb06
Andrew Menzl <Menzla@midlandps.org>
2017-02-06 20:32:25 -0500

    Renamed EVERYTHING.

---
+++ src/org/usfirst/frc2619/SteamBot2017/MathUtil.java
@@ -0,0 +1,62 @@
+package org.usfirst.frc2619.SteamBot2017;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class MathUtil {
+	public static boolean Allow_Delinearization;
+	public static final boolean ALLOW_DELINEARIZATION_DEFAULT = true;
+	public static final String ALLOW_DELINEARIZATION_KEY = "Allow_Delinearization";
+	
+	public static double delinearize(double input, int power) {
+		double dbY = Robot.driveTrain.deadband_y;
+		double ret;
+		Allow_Delinearization = false;
+		TheChargeDashboard.putNumber("JoystickPosition", input);
+        TheChargeDashboard.putBoolean(ALLOW_DELINEARIZATION_KEY, Allow_Delinearization);
+		Allow_Delinearization = SmartDashboard.getBoolean(ALLOW_DELINEARIZATION_KEY, ALLOW_DELINEARIZATION_DEFAULT);
+		
+		
+		if(Allow_Delinearization == true) {
+		//returns delinearized power (adjusted for deadband)
+			if(input > 0) {
+				//this is an equation for the curve so that it starts at zero at the edge of the deadband...
+				//...and goes up cubically to (1,1) - this will work even if we change the deadband
+				ret = 1/Math.pow(1 - dbY, power) * Math.pow(input - dbY, power);
+			}
+			else if (input < 0) {
+				//this is the reverse of that equation, in order to make the negative go backward
+				ret = 1/Math.pow(1 - dbY, power) * Math.pow(input + dbY, power);
+			}
+			else {
+				ret = 0;
+			}
+		}
+		else {
+			//if delin modifications are off, will return non-fixed delinearization
+			ret = Math.pow(input, power);
+			ret = input;
+		}
+		
+		TheChargeDashboard.putNumber("PowerOutput", ret);
+		return ret;
+		
+	}
+
+	public static double deadbandCheck(double input, double db) {
+		
+		// ---------if----- =then=
+		return Math.abs(input) < db ? 0 : input;
+		// --else-
+	}
+	public static double calcDirection(double current, double desired) {
+		current = Math.toRadians(current);
+		desired = Math.toRadians(desired);
+		double current_x = Math.cos(current);
+		double current_y = Math.sin(current);
+		double desired_x = Math.cos(desired);
+		double desired_y = Math.sin(desired);
+		double direction = Math.signum(current_x * desired_y - desired_x
+				* current_y);
+		return direction;
+	}
+}
---
+++ src/org/usfirst/frc2619/SteamBot2017/TheChargeDashboard.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc2619.SteamBot2017;
+
+import org.usfirst.frc2619.SteamBot2017.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.NamedSendable;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TheChargeDashboard {
+
+	private static String[] whiteList = {
+			DriveTrain.DELIN_POW_KEY, DriveTrain.DEADBAND_X_KEY, DriveTrain.DEADBAND_Y_KEY, DriveTrain.DEADBAND_TWIST_KEY,
+			"PowerOutput", "JoystickPosition", "Allow_Delinearization",
+			"TURN_OUTER_SPEED", "TURN_INNER_SPEED", "FinalDegrees",
+			"CURRENT_LIMIT",
+			"POSITION_P", "POSITION_I", "POSITION_D", "POSITION_F", "ACCELERATION", "VELOCITY", "DISTANCE",
+			"IMU_TotalYaw", "IMU_YawRateDPS", "IMU_Connected", "IMU_IsCalibrating", "IMU_Yaw", "IMU_Pitch", "IMU_Roll", "IMU_Byte_Count", "IMU_Update_Count",
+			"Setpoint",
+			"LeftSpeed", "RightSpeed",
+			"Direction",
+			//"ShooterSpeedP", "ShooterSpeedI", "ShooterSpeedD", "ShooterSpeedF",
+			"driveTrain",
+			"AutoMode"
+			};
+
+	public static void putNumber(String key, double value) {
+		for (int x = 0; x < whiteList.length; x++) {
+			if (key.equals(whiteList[x])) {
+				SmartDashboard.putNumber(key, value);
+			}
+		}
+	}
+
+	public static void putBoolean(String key, boolean value) {
+		for (int x = 0; x < whiteList.length; x++) {
+			if (key.equals(whiteList[x])) {
+				SmartDashboard.putBoolean(key, value);
+			}
+		}
+	}
+
+	public static void putString(String key, String value) {
+		for (int x = 0; x < whiteList.length; x++) {
+			if (key.equals(whiteList[x])) {
+				SmartDashboard.putString(key, value);
+			}
+		}
+	}
+	
+	public static void putData(String key, Sendable value) {
+		for (int x = 0; x < whiteList.length; x++) {
+			if (key.equals(whiteList[x])) {
+				SmartDashboard.putData(key, value);
+			}
+		}
+	}
+	
+	public static void putData(NamedSendable value) {
+		for (int x = 0; x < whiteList.length; x++) {
+			if (value.getName().equals(whiteList[x])) {
+				SmartDashboard.putData(value);
+			}
+		}
+	}
+}
