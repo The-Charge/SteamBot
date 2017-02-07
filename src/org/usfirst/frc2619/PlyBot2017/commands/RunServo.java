daeba7193419905085f521df0e8c6142c9b47d8a
Andrew Menzl <Menzla@midlandps.org>
2017-02-06 19:15:41 -0500

    Changed ESForward and ESOff to instantaneous commands, removed static references.

--- src/org/usfirst/frc2619/PlyBot2017/commands/RunServo.java
+++ src/org/usfirst/frc2619/PlyBot2017/commands/RunServo.java
@@ -43,8 +43,8 @@ public class RunServo extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	double output = Robot.oi.getButtonBox().getRawAxis(3);
-    	Servos.servoRun(output);
-    }    	    	
+    	Robot.servos.servoRun(output);
+    }
 
 
     // Make this return true when this Command no longer needs to run execute()
