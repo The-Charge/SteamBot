7c252776d871d66310dac100a4faba9bf56db691
Andrew Menzl <Menzla@midlandps.org>
2017-03-10 19:07:07 -0500

    Added code to non-shooting autons to drive closer to the gear loading station. Needs LOTS of testing.

--- src/org/usfirst/frc2619/SteamBot2017/commands/GearPegLeftAutonRed.java
+++ src/org/usfirst/frc2619/SteamBot2017/commands/GearPegLeftAutonRed.java
@@ -49,5 +49,7 @@ public class GearPegLeftAutonRed extends CommandGroup {
     	//addSequential(new DeliverGearWithVision());
     	addSequential(new DriveToCurrent(0.1));
     	addSequential(new DeliverGear());
+    	addSequential(new TurnNDegreesAbsolutePID(0));
+    	addSequential(new DriveXFeetMM(35));
     } 
 }
