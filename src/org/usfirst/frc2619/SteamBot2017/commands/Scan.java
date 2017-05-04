397a08d3ca67b0c941ad33b2efa37ed38bc66c6e
Andrew Menzl <ammenzl7@gmail.com>
2017-05-04 18:32:51 -0400

    OCD

--- src/org/usfirst/frc2619/SteamBot2017/commands/Scan.java
+++ src/org/usfirst/frc2619/SteamBot2017/commands/Scan.java
@@ -40,14 +40,11 @@ public class Scan extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-
 		// RobotMap.ultrasonicUltrasonicSensor.setOversampleBits(bits);
-
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
